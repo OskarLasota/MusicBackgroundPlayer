c16b752857be6c17ed0eb70183100be39a553eff
Oskar Lasota <55768328+OskarLasota@users.noreply.github.com>
2020-09-06 18:54:27 +0100

    chore: getting all songs from downloads folder + request permissions

--- app/src/main/java/com/frezzcoding/musicplayer/MainActivity.kt
+++ app/src/main/java/com/frezzcoding/musicplayer/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.frezzcoding.musicplayer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var mediaPlayer : MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE), PackageManager.PERMISSION_GRANTED)
+
+        var listOfFiles = File("/sdcard/Download")
+        var listOfSongs : ArrayList<File> = arrayListOf()
+        for(file in listOfFiles.listFiles()){
+            if(file.name.contains("mp3") || file.name.contains("mp4")) {
+                listOfSongs.add(file)
+                println(file.path)
+            }
+        }
+
+        /*
+        TODO We want a recycler view with all listOfSongs
+        TODO 3 buttons , pause start stop
+        TODO play in background
+         */
+
+
+        mediaPlayer = MediaPlayer.create(this, Uri.fromFile(listOfSongs[1]))
+        mediaPlayer.start()
+        println("soungs found ${listOfSongs.size}")
+
+
     }
 }
